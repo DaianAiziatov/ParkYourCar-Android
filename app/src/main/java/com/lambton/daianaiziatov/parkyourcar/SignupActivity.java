6cc8bd2c0f620111186cab2f2a57878fa235ba35
Daian <daian.aiziatov@gmail.com>
2018-11-25 13:02:19 -0500

    preparation for signup screen

--- app/src/main/java/com/lambton/daianaiziatov/parkyourcar/SignupActivity.java
+++ app/src/main/java/com/lambton/daianaiziatov/parkyourcar/SignupActivity.java
@@ -1,13 +1,106 @@
 package com.lambton.daianaiziatov.parkyourcar;
 
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEdittext;
+    private EditText numberEditText;
+    private EditText passwordEditText;
+    private EditText passwordConfirmationEditText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firstNameEditText = findViewById(R.id.user_first_name_edit_view);
+        lastNameEditText = findViewById(R.id.user_last_name_edit_view);
+        emailEdittext = findViewById(R.id.user_email_edit_view);
+        numberEditText = findViewById(R.id.user_number_edit_view);
+        passwordEditText = findViewById(R.id.user_password_edit_view);
+        passwordConfirmationEditText = findViewById(R.id.user_password_confirmation_edit_view);
+    }
+
+    public void signUp(View view) {
+        if (areAllFieldsFilled()) {
+            if (isPasswordValid()) {
+                String email = emailEdittext.getText().toString();
+                String password = passwordEditText.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(SignupActivity.this, "User created successfully.", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference myRef = database.getReference("users/" + user.getUid());
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignupActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            } else {
+                showAlertWithMessage("Please check passwords fields");
+            }
+        } else {
+            showAlertWithMessage("Please fill all fields.");
+        }
+    }
+
+    private boolean areAllFieldsFilled() {
+        boolean result = !firstNameEditText.getText().toString().isEmpty() &&
+                !lastNameEditText.getText().toString().isEmpty() &&
+                !emailEdittext.getText().toString().isEmpty() &&
+                !numberEditText.getText().toString().isEmpty() &&
+                !passwordEditText.getText().toString().isEmpty() &&
+                !passwordConfirmationEditText.getText().toString().isEmpty();
+        return result;
+    }
+
+    private boolean isPasswordValid() {
+        if (!passwordEditText.getText().toString().isEmpty() && !passwordConfirmationEditText.getText().toString().isEmpty()) {
+            String password = passwordEditText.getText().toString();
+            String passwordConfirmation = passwordConfirmationEditText.getText().toString();
+            return password.equals(passwordConfirmation);
+        } else {
+            return false;
+        }
+    }
+
+    private void showAlertWithMessage(String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(SignupActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
