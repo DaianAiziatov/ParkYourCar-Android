a82c44bec70082ed51c53efa9aad8b64b8149f6b
Daian <daian.aiziatov@gmail.com>
2018-12-12 00:13:26 -0500

    Update profile implemented

--- app/src/main/java/com/lambton/daianaiziatov/parkyourcar/UpdateProfileFragment.java
+++ app/src/main/java/com/lambton/daianaiziatov/parkyourcar/UpdateProfileFragment.java
@@ -4,6 +4,7 @@ package com.lambton.daianaiziatov.parkyourcar;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
@@ -49,7 +50,7 @@ public class UpdateProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View updateView = inflater.inflate(R.layout.fragment_update_profile, container, false);
+        final View updateView = inflater.inflate(R.layout.fragment_update_profile, container, false);
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
@@ -64,11 +65,37 @@ public class UpdateProfileFragment extends Fragment {
         newPasswordEditText = updateView.findViewById(R.id.new_password_edit_view);
         confirmPasswordEditText = updateView.findViewById(R.id.confirm_password_edit_view);
         updateButton = updateView.findViewById(R.id.update_button);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
+            }
+        });
 
         emailTextView.setText("Email: " + user.getEmail());
         loadUser();
 
         return updateView;
+    }
+
+    private void update() {
+        final String firstName = firstNameEditText.getText().toString();
+        final String lastName = lastNameEditText.getText().toString();
+        final String contactNumber = contactEditText.getText().toString();
+        Map<String, Object> userUpdate = new HashMap<>();
+        userUpdate.put("firstName", firstName);
+        userUpdate.put("lastName", lastName);
+        userUpdate.put("contactNumber", contactNumber);
+        myRef.updateChildren(userUpdate, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    showAlertWithMessage("User profile updated successfully!");
+                } else {
+                    showAlertWithMessage(databaseError.getMessage());
+                }
+            }
+        });
     }
 
     private void loadUser() {
